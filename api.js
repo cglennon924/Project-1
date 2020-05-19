4abc127cc71a2ecbee7c9836d52724e2b35c6340
Christian Gomez Martinez <gomez9289@gmail.com>
2020-05-19 08:14:29 -0400

    modified wikipedia

--- api.js
+++ api.js
@@ -1,40 +1,49 @@
+// var MM_API_KEY = "9878e4e3e1c98453316ef422d8148877"; // MusixMatch API key
+
+// var queryString = window.location.search;
+// var urlParams = new URLSearchParams(queryString);
+// var keyword = urlParams.get("keyword");
+
+// function queryMusixMatch() {
+//   var queryURL =
+//     "https://api.musixmatch.com/ws/1.1/track.search?q=" +
+//     keyword +
+//     "&apikey=" +
+//     MM_API_KEY;
+
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET",
+//   }).then(function (response) {
+//     response = JSON.parse(response);
+
+//     var firstTrack = response.message.body.track_list[0].track;
+//     console.log(firstTrack.track_id);
+
+//     var nextQueryURL =
+//       "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=" +
+//       firstTrack.track_id +
+//       "&apikey=" +
+//       MM_API_KEY;
+
+//     $.ajax({
+//       url: nextQueryURL,
+//       method: "GET",
+//     }).then(function (nextResponse) {
+//       nextResponse = JSON.parse(nextResponse);
+//       var lyrics = nextResponse.message.body.lyrics.lyrics_body;
+//       var lyricsWithBreaks = lyrics.split("\n").join("<br>");
+//       $("#mm-results").html(lyricsWithBreaks);
+//     });
+//   });
+// }
+// queryMusixMatch();
 
 
+// function queryWikipedia() {
 
 
-
-//----------Wikipedia-----------
-
-$(function() {
-    $("#search-musician").on("keycode", function(e){
-        if (e.keycode===13){
-            $("#submit").on("click", function(){
-
-                $( "#clear" ).click(function() {
-                    $( "#output" ).empty();
-                  });
-            
-                var artist = $("textarea").val();
-                var queryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ artist +"&format=json&callback=?";
-        
-                $.ajax({
-                    url: queryURL,
-                    method: "GET",
-                    contentType: "application/json; charset=utf-8",
-                    async: false,
-                    dataType: "json",
-                    success: function(data, status, jqXHR) {
-                        $("#output").html();
-                        for(var i=0; i < data[1].length-1; i++) {
-                            $("#output").append("<div><div class='btn-primary'><a href=" +data[3][i]+ "><h2>" +data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
-                        console.log(data);
-                        }
-                    }
-                })
-
-            });
-        }
-    });
+//--------------------------------------------------
 
     $("#submit").on("click", function(){
 
@@ -43,8 +52,9 @@ $(function() {
             
         });
     
-        var artist = $("textarea").val();
-        var queryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ artist +"&format=json&callback=?";
+        var searchTerm = $(".textarea").val();
+        var queryURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+        //var queryURL = "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&search=" + searchTerm + "&namespace=14%7C-1&limit=5&profile=engine_autoselect";
 
         $.ajax({
             url: queryURL,
@@ -54,8 +64,8 @@ $(function() {
             dataType: "json",
             success: function(data, status, jqXHR) {
                 $("#output").html();
-                for(var i=0; i < data[1].length-1; i++) {
-                    $("#output").append("<div><div class='btn-primary'><a href=" +data[3][i]+ "><h2>" +data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+                for(var i=0; i < data[1].length-8; i++) {
+                    $("#output").append("<div><div class='btn-primary'><a href=" +data[1][i]+ "><h2>" +data[1][i]+ "</h2>" + "<p>" + data[3][i] + "</p></a></div></div>");
                 console.log(data);
                 }
             }
@@ -70,7 +80,9 @@ $(function() {
             console.log("complete");
         });
     });
-});
+
+//queryWikipedia();
+
     
     
 
@@ -80,42 +92,42 @@ $(function() {
 
 //---------Giphy----------
 
-function displayInfo() {
-$("submit").on("click", function() {
+// function displayInfo() {
+// $("submit").on("click", function() {
     
 
-var person = $(this).attr("#search-musician");
+// var person = $(this).attr("#search-musician");
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=LQF6WEjUOrbemyHpMNLZA3bw8L7O4UYF";
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=LQF6WEjUOrbemyHpMNLZA3bw8L7O4UYF";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    $("p").text(JSON.stringify(response));
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//     $("p").text(JSON.stringify(response));
 
-    var results = response.data;
+//     var results = response.data;
 
-    for (var i = 0; i < results.length; i++) {
-    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+//     for (var i = 0; i < results.length; i++) {
+//     if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
-    var gifDiv = $("<div>");
+//     var gifDiv = $("<div>");
 
-    var personImage = $("<img>");
+//     var personImage = $("<img>");
 
-    personImage.attr("src", results[i].images.fixed_height.url);
+//     personImage.attr("src", results[i].images.fixed_height.url);
 
-    gifDiv.append(personImage);
+//     gifDiv.append(personImage);
  
-    $("p").appendTo(gifDiv);
-        }
-    }
-});
-});
+//     $("p").appendTo(gifDiv);
+//         }
+//     }
+// });
+// });
 
 
-}
+// }
 
 
 
